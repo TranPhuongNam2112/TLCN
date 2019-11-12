b84ad288b92e24824108269223240cf8ddfd86fa
TranPhuongNam2112 <34429397+TranPhuongNam2112@users.noreply.github.com>
2019-11-12 15:12:08 +0700

    Upload

---
+++ webkn/src/views/Register.vue
@@ -0,0 +1,115 @@
+<template>
+    <section class="section section-shaped section-lg my-0">
+        <div class="shape shape-style-1 bg-gradient-default">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="container pt-lg-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <card type="secondary" shadow
+                          header-classes="bg-white pb-5"
+                          body-classes="px-lg-5 py-lg-5"
+                          class="border-0">
+                        <template>
+                            <div class="text-muted text-center mb-3">
+                                <small>Sign in with</small>
+                            </div>
+                            <div class="btn-wrapper text-center">
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/github.svg">
+                                    Github
+                                </base-button>
+
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/google.svg">
+                                    Google
+                                </base-button>
+                            </div>
+                        </template>
+                        <template>
+                            <div class="text-center text-muted mb-4">
+                                <small>Or sign up with credentials</small>
+                            </div>
+                            <form role="form">
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Name"
+                                            addon-left-icon="ni ni-hat-3"
+                                            v-model="newUser.username">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Email"
+                                            addon-left-icon="ni ni-email-83"
+                                            v-model="newUser.email">
+                                </base-input>
+                                <base-input alternative
+                                            type="password"
+                                            placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open"
+                                            v-model="newUser.password">
+                                </base-input>
+                                 <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Name"
+                                            addon-left-icon="ni ni-hat-3"
+                                            v-model="newUser.phone">
+                                </base-input>
+                                
+                               
+                                <div class="text-center">
+                                    <base-button type="primary" class="my-4" @click="registerUser">Create account</base-button>
+                                </div>
+                            </form>
+                        </template>
+                    </card>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+import web3 from '../../contracts/web3';
+import Users from '../../contracts/usersInstance';
+export default {
+data() {
+    return {
+      startProjectDialog: false,
+      account: null,
+      stateMap: [
+        { color: 'primary', text: 'Ongoing' },
+        { color: 'warning', text: 'Expired' },
+        { color: 'success', text: 'Completed' },
+      ],
+      projectData: [],
+      newProject: { isLoading: false },
+      newUser: {},
+    };
+  },
+methods: {
+    registerUser() {
+      Users.methods.registerUser(
+        this.newUser.username,
+        this.newUser.password,
+        this.newUser.email,
+        this.newUser.phone,
+      ).send({
+        from: this.account,
+      }).then((res) => {
+        const UserInfo = res.events.newUserRegistered.returnValues;
+      })
+    }
+}
+
+};
+
+</script>
+<style>
+</style>
